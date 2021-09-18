c8127f9169698505798d61d05078f2bda5981729
Wruzz Jen-Iisk <43224790+GentlePuppet@users.noreply.github.com>
2021-09-18 14:35:40 -0400

    Version 1.2
    - Fixed avatars and thumbnails not being replaced.

--- Youtube HD Avatars & Thumbnails/Youtube HD Avatars & Thumbnails.user.js
+++ Youtube HD Avatars & Thumbnails/Youtube HD Avatars & Thumbnails.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Youtube HD Avatars & Thumbnails
 // @namespace    http://tampermonkey.net/
-// @version      1.1
+// @version      1.2
 // @author       GentlePuppet
 // @match        https://www.youtube.com/*
 // @icon         https://www.youtube.com/s/desktop/1eca3218/img/favicon_144.png
@@ -20,20 +20,22 @@ GM_addStyle('#author-thumbnail.ytd-comment-renderer yt-img-shadow.ytd-comment-re
 // Hover for Bigger Avatar | Comment Replies
 GM_addStyle('ytd-comment-renderer:not([comment-style=backstage-comment])[is-reply] #author-thumbnail.ytd-comment-renderer yt-img-shadow.ytd-comment-renderer:hover, ytd-comment-renderer[is-creator-reply] #author-thumbnail.ytd-comment-renderer yt-img-shadow.ytd-comment-renderer:hover {height: 150px; width: 150px; transition: height 0.5s, width 0.5s;} ytd-comment-renderer:not([comment-style=backstage-comment])[is-reply] #author-thumbnail.ytd-comment-renderer yt-img-shadow.ytd-comment-renderer, ytd-comment-renderer[is-creator-reply] #author-thumbnail.ytd-comment-renderer yt-img-shadow.ytd-comment-renderer {height: 24px; width: 24px; transition: height 0.5s, width 0.5s;}');
 
-// Avatars
-waitForKeyElements ('yt-img-shadow > img[src*="no-rj-mo"]', Avatar, 0);
-function Avatar (jNode) {
-    var oldSrc = jNode.attr ("src");
-    let newSrc = oldSrc.replace (/=s.*-no-rj-mo/gi, "");
-    if (newSrc.width == 0) {return;}
-    jNode.attr ("src", newSrc);
-};
+window.addEventListener("yt-page-data-updated", function(e) {
+    // Avatars
+    waitForKeyElements ('yt-img-shadow > img[src*="=s176"]', Avatar, 0);
+    waitForKeyElements ('yt-img-shadow > img[src*="=s88"]', Avatar, 0);
+    waitForKeyElements ('yt-img-shadow > img[src*="=s48"]', Avatar, 0);
+    function Avatar (jNode) {
+        var oldSrc = jNode.attr ("src");
+        let newSrc = oldSrc.replace (/=s\d*.*/gi, "");
+        jNode.attr ("src", newSrc);
+    };
 
-// Video Thumbnails
-waitForKeyElements ('ytd-thumbnail #thumbnail.ytd-thumbnail yt-img-shadow.ytd-thumbnail > img[src*="hqdefault.jpg"]', Thumbnail, 0);
-function Thumbnail (jNode) {
-    var oldSrc2 = jNode.attr ("src");
-    let newSrc2 = oldSrc2.replace ("hqdefault.jpg", "maxresdefault.jpg");
-    if (newSrc2.width == 0) {return;}
-    jNode.attr ("src", newSrc2);
-};
+    // Video Thumbnails
+    waitForKeyElements ('ytd-thumbnail #thumbnail.ytd-thumbnail yt-img-shadow.ytd-thumbnail > img[src*="hqdefault.jpg"]', Thumbnail, 0);
+    function Thumbnail (jNode) {
+        var oldSrc2 = jNode.attr ("src");
+        let newSrc2 = oldSrc2.replace ("hqdefault.jpg", "maxresdefault.jpg");
+        jNode.attr ("src", newSrc2);
+    };
+});
