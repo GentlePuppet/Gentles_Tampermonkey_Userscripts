05b2439c627974b9c54114473e098ef9847e2cb4
Wruzz Jen-Iisk <43224790+GentlePuppet@users.noreply.github.com>
2021-09-22 14:16:29 -0400

    Version 1.1
    Fixed the script not finding the tweeter.

--- Twitter Hide Self-Retweets/Twitter_Remove_Self-Retweets.user.js
+++ Twitter Hide Self-Retweets/Twitter_Remove_Self-Retweets.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Twitter Remove Self-Retweets
 // @author       GentlePuppet
-// @version      1.0
+// @version      1.1
 // @include      https://twitter.com/*
 // @run-at       document-start
 // @require      https://cdn.jsdelivr.net/npm/jquery@3.4.1/dist/jquery.min.js
@@ -11,7 +11,7 @@
 waitForKeyElements (`span[data-testid="socialContext"]:contains('Retweeted')`, CheckForSelfRetweet, 0);
 function CheckForSelfRetweet (jnode) {
     var Retweeted = jnode.parent().attr('href');
-    var Retweeter = jnode.parents('article[role="article"]').find('div[data-testid="tweet"] > div:nth-child(1) > div > div > a[role="link"]').attr('href');
+    var Retweeter = jnode.parents('article[role="article"]').find('article > div > div > div > div:nth-child(2) > div:nth-child(2) > div:nth-child(1) > div > div > div:nth-child(1) > div:nth-child(1) > a[role="link"]').attr('href');
 
     if (Retweeter == Retweeted) {
         jnode.parents('article[role="article"]').parent().parent().parent().hide();
