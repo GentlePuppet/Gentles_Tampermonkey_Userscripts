2b8fe8859767816e35c0c7333517f84259fa0444
Wruzz Jen-Iisk <43224790+GentlePuppet@users.noreply.github.com>
2021-09-22 23:13:05 -0400

    Version 1.2
    Actually fixed the script not finding the retweeter.

--- Twitter Hide Self-Retweets/Twitter_Remove_Self-Retweets.user.js
+++ Twitter Hide Self-Retweets/Twitter_Remove_Self-Retweets.user.js
@@ -1,19 +1,19 @@
 // ==UserScript==
 // @name         Twitter Remove Self-Retweets
 // @author       GentlePuppet
-// @version      1.1
+// @version      1.2
 // @include      https://twitter.com/*
 // @run-at       document-start
 // @require      https://cdn.jsdelivr.net/npm/jquery@3.4.1/dist/jquery.min.js
 // @require      https://gist.github.com/raw/2625891/waitForKeyElements.js
 // ==/UserScript==
 
-waitForKeyElements (`span[data-testid="socialContext"]:contains('Retweeted')`, CheckForSelfRetweet, 0);
+waitForKeyElements(`span[data-testid="socialContext"]:contains('Retweeted')`, CheckForSelfRetweet, 0);
 function CheckForSelfRetweet (jnode) {
     var Retweeted = jnode.parent().attr('href');
-    var Retweeter = jnode.parents('article[role="article"]').find('article > div > div > div > div:nth-child(2) > div:nth-child(2) > div:nth-child(1) > div > div > div:nth-child(1) > div:nth-child(1) > a[role="link"]').attr('href');
-
+    var Retweeter = jnode.parents('article[role="article"]').find('div > div:nth-child(1) > div:nth-child(1) > a[role="link"]').attr('href');
     if (Retweeter == Retweeted) {
         jnode.parents('article[role="article"]').parent().parent().parent().hide();
+        return;
     }
 }
