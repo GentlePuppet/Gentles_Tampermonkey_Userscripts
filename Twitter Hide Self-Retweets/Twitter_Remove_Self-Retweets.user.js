30228694b99d2fa8153d6b8c0f0ddac14dc2d4d4
Wruzz Jen-Iisk <43224790+GentlePuppet@users.noreply.github.com>
2021-10-31 19:04:59 -0400

    Final Version
    This script has been merged with https://github.com/GentlePuppet/Gentles_Tampermonkey_Userscripts/raw/main/Better%20Twitter/Better%20Twitter.user.js

--- Twitter Hide Self-Retweets/Twitter_Remove_Self-Retweets.user.js
+++ Twitter Hide Self-Retweets/Twitter_Remove_Self-Retweets.user.js
@@ -1,19 +1,8 @@
 // ==UserScript==
 // @name         Twitter Remove Self-Retweets
 // @author       GentlePuppet
-// @version      1.2
-// @include      https://twitter.com/*
-// @run-at       document-start
-// @require      https://cdn.jsdelivr.net/npm/jquery@3.4.1/dist/jquery.min.js
-// @require      https://gist.github.com/raw/2625891/waitForKeyElements.js
+// @version      Removed
 // ==/UserScript==
 
-waitForKeyElements(`span[data-testid="socialContext"]:contains('Retweeted')`, CheckForSelfRetweet, 0);
-function CheckForSelfRetweet (jnode) {
-    var Retweeted = jnode.parent().attr('href');
-    var Retweeter = jnode.parents('article[role="article"]').find('div > div:nth-child(1) > div:nth-child(1) > a[role="link"]').attr('href');
-    if (Retweeter == Retweeted) {
-        jnode.parents('article[role="article"]').parent().parent().parent().hide();
-        return;
-    }
-}
+// Script merged with "https://github.com/GentlePuppet/Gentles_Tampermonkey_Userscripts/raw/main/Better%20Twitter/Better%20Twitter.user.js"
+// Please Delete this Script
