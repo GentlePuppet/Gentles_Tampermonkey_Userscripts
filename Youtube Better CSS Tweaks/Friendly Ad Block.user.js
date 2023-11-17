c5ede30ee4c14b53393551926bb4eca232914622
Wruzz Jen-Iisk <43224790+GentlePuppet@users.noreply.github.com>
2023-11-17 13:02:02 -0500

    Update Friendly Ad Block.user.js
    - Added a more strict selector
    - Added code comments

--- Youtube Better CSS Tweaks/Friendly Ad Block.user.js
+++ Youtube Better CSS Tweaks/Friendly Ad Block.user.js
@@ -1,41 +1,50 @@
 // ==UserScript==
 // @name         Gentle's Ad Block
-// @version      1.2.1
+// @version      1.3
 // @author       Originally by 0x48piraj | Converted to a simpified userscript by Gentle
 // @match        https://www.youtube.com/*
 // @icon         https://www.youtube.com/s/desktop/1eca3218/img/favicon_144.png
-// @description  Friendly Ad Block is an extension created by 0x48piraj @https://github.com/0x48piraj/fadblock/tree/master. Converted into a simple userscript by Gentle.
+// @description  A simple script to quickly skip any ads that start playing. It doesn't block ads so it doesn't get detected.
 // @require      https://code.jquery.com/jquery-3.7.1.min.js
 // @run-at       document-start
 // @updateURL    https://github.com/GentlePuppet/Gentles_Tampermonkey_Userscripts/raw/main/Youtube%20Better%20CSS%20Tweaks/Friendly%20Ad%20Block.user.js
 // @downloadURL  https://github.com/GentlePuppet/Gentles_Tampermonkey_Userscripts/raw/main/Youtube%20Better%20CSS%20Tweaks/Friendly%20Ad%20Block.user.js
 // ==/UserScript==
 
-var script = document.createElement('script'); script.src = "https://code.jquery.com/jquery-3.7.1.min.js"; document.getElementsByTagName('head')[0].appendChild(script);
-
+// Create TimerActive Variable.
 let TimerActive = false;
 
+// Attach Adtimer function to the page update event to restart the timer if the page changes.
 window.addEventListener("yt-page-data-updated", function() {AdTimer();})
 
+// The Adtimer function that is the core of the
 function AdTimer() {
-    const video = $("video")
+    // If the timer isn't already running start it.
     if (TimerActive == false) {
         TimerActive = true;
+        // Loop every 100ms to check if an ad is playing.
         setInterval(function() {
+            // If an ad is showing continue
             if ($(".ad-showing") || $(".ad-interrupting")) {
-                video.currentTime = video.duration - 0.1
+                // Set the video's current time to the last 0.1 second and then click the skip buttons if they exist.
+                $("video")[0].currentTime = $("video")[0].duration - 0.1;
+                // Pass these selectors to the click button function.
                 ClickAdButton($(".ytp-ad-skip-button"));
                 ClickAdButton($(".ytp-ad-skip-button-modern"));
             }
         },100);
     }
-}
-AdTimer()
+};
 
+// Function to click the passed through element
 function ClickAdButton(e) {
+    // If the button doesn't exist end the function early, otherwise click the button.
     if (e == null) {return} else {e.click()}
 }
 
+
+// This block is from Friendly Ad Block created by 0x48piraj @https://github.com/0x48piraj/fadblock/tree/master.
+// Converted to a userscript by Gentle.
 window.addEventListener("yt-page-data-updated", function(e) {
     function HideAdLoop() {
         const staticAds = [".ytd-companion-slot-renderer", ".ytd-action-companion-ad-renderer", // in-feed video ads
@@ -48,5 +57,6 @@ window.addEventListener("yt-page-data-updated", function(e) {
                           ];
         staticAds.forEach((ad) => {document.querySelectorAll(ad).forEach((el) => (el.style.display = "none"))});
     }
+    // Wait 1 second then hide all the elements listed above.
     setTimeout(HideAdLoop, 1000);
 });
