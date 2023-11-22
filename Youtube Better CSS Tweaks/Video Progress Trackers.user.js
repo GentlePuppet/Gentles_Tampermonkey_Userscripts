37bc87b32b7f1c2dc78aab9986567dbd36859418
Wruzz Jen-Iisk <43224790+GentlePuppet@users.noreply.github.com>
2023-11-21 20:33:08 -0500

    Update Video Progress Trackers.user.js

--- Youtube Better CSS Tweaks/Video Progress Trackers.user.js
+++ Youtube Better CSS Tweaks/Video Progress Trackers.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Youtube Gentle's Youtube Video Progress Trackers
-// @version      1.6
+// @version      1.7
 // @author       GentlePuppet
 // @description  Adds video progression displays
 // @match        https://www.youtube.com/*
@@ -14,58 +14,75 @@
 GM_addStyle(`
     #container.ytd-player:has(.ytp-fullscreen) > .DummyContainerProgress {margin-top:-7px !important;}
     #container.ytd-player:not(.ytp-fullscreen) > .DummyContainerProgress {margin-top: 0px;}
+    .DummyContainerProgress {width: 100%; height: 3px; outline: none; padding-top: 2px;}
+    .DummyPlayProgress {width: 100%; height: 3px; position: absolute; left: 0px; transform-origin: 0 0; z-index: 3; background-color: #f00;}
+    .DummyLoadProgress {width: 100%; height: 3px; position: absolute; left: 0px; transform-origin: 0 0; z-index: 2; background: rgba(255,255,255,0.4);}
+    .DummyBackgroundProgress {width: 100%; height: 3px; position: absolute; left: 0px; transform-origin: 0 0; z-index: 1; background: rgba(90,90,90,0.4);}
+    .VTLC {display: block; min-width: fit-content; overflow: hidden; font-weight: 500; color: #f1f1f1; font-size: 14px; line-height: 36px; background-color: var(--yt-spec-badge-chip-background); padding: 0px 10px; border-radius: 2px; margin-left: 8px;}
 `);
 
 window.addEventListener("yt-page-data-updated", function(e) {
-    // Mini Progress Bar
-    // Created by Gentle
     if(window.location.href.indexOf("watch") !==-1 ) {
-        const DCP = $('<div/>').attr({class: "DummyContainerProgress", style: "width: 100%; height: 3px; outline: none; padding-top: 2px; margin-top: 0px;"});
-        const DPP = $('<div/>').attr({class: "DummyPlayProgress", style: "transform: scaleX(0);width: 100%; height: 3px; position: absolute; left: 0px; transform-origin: 0 0; z-index: 3; background-color: #df6eed;"});
-        const DLP = $('<div/>').attr({class: "DummyLoadProgress", style: "transform: scaleX(0);width: 100%; height: 3px; position: absolute; left: 0px; transform-origin: 0 0; z-index: 2; background: rgba(255,255,255,0.4);"});
-        const DBP = $('<div/>').attr({class: "DummyBackgroundProgress", style: "width: 100%; height: 3px; position: absolute; left: 0px; transform-origin: 0 0; z-index: 1; background: rgba(90,90,90,0.4);"});
+        // Mini Progress Bar
+        const DCP = $('<div/>').attr({class: "DummyContainerProgress"})
+        const DPP = $('<div/>').attr({class: "DummyPlayProgress", style: "transform: scaleX(0);"})
+        const DLP = $('<div/>').attr({class: "DummyLoadProgress", style: "transform: scaleX(0);"})
+        const DBP = $('<div/>').attr({class: "DummyBackgroundProgress"})
         function createdummytimer() {
             if (document.querySelector(".DummyContainerProgress") == null) {
-                DCP.insertAfter('.html5-video-player');
-                $('.DummyContainerProgress').append(DPP);
-                $('.DummyContainerProgress').append(DLP);
-                $('.DummyContainerProgress').append(DBP);
-                UpdateDummyProgress();
-                requestAnimationFrame(createdummytimer);
-            } else {requestAnimationFrame(createdummytimer)}
+                DCP.insertAfter('.html5-video-player')
+                $('.DummyContainerProgress').append(DPP)
+                $('.DummyContainerProgress').append(DLP)
+                $('.DummyContainerProgress').append(DBP)
+                requestAnimationFrame(createdummytimer)
+            } else {
+                requestAnimationFrame(createdummytimer)
+            }
         }
         requestAnimationFrame(createdummytimer)
-    }
-    // Update Mini Progress Bar
-    // Original Code Created by Nemanja Bu for "Always Show Progress Bar" | Modified for Gentle's "Mini Progress Bar"
-    // https://greasyfork.org/en/scripts/30046-youtube-always-show-progress-bar/code
-    function UpdateDummyProgress() {
-        function timeup() {progressbar.style.transform = "scaleX("+(video.currentTime/video.duration)+")";}
-        function progup() {if (!video.buffered.length) {return;};var buffer = video.buffered.end(video.buffered.length-1)/video.duration;if(buffer < 0){return};loadbar.style.transform = "scaleX("+buffer+")";}
-        document.querySelector(".video-stream").removeEventListener("timeupdate", timeup, true);
-        document.querySelector(".video-stream").removeEventListener("progress", progup, true);
-        $('.DummyPlayProgress').attr({style: "transform: scaleX(0);width: 100%; height: 3px; position: absolute; left: 0px; transform-origin: 0 0; z-index: 3; background-color: #df6eed;"});
-        $('.DummyLoadProgress').attr({style: "transform: scaleX(0);width: 100%; height: 3px; position: absolute; left: 0px; transform-origin: 0 0; z-index: 2; background: rgba(255,255,255,0.4);"});
-        const video = document.querySelector(".video-stream");
-        const progressbar = document.querySelector(".DummyPlayProgress");
-        const loadbar = document.querySelector(".DummyLoadProgress");
-        function findVideoInterval() {
-            if (!video || !progressbar || !loadbar) {requestAnimationFrame(findVideoInterval)}
-            video.addEventListener("timeupdate", timeup, true);
-            video.addEventListener("progress", progup, true);
+        function UpdateMiniBar() {
+            function timeup() {
+                var dummy = document.querySelector(".DummyPlayProgress")
+                if (!dummy) {return}
+                var video = document.querySelector(".video-stream.html5-main-video")
+                dummy.style.transform = "scaleX("+(video.currentTime/video.duration)+")"
+            }
+            function progup() {
+                var dummy = document.querySelector(".DummyLoadProgress")
+                if (!dummy) {return}
+                var video = document.querySelector(".video-stream.html5-main-video")
+                if (!video.buffered.length) {return}
+                var buffer = video.buffered.end(video.buffered.length-1)/video.duration
+                if(buffer < 0){return}
+                dummy.style.transform = "scaleX("+buffer+")"
+            }
+            const player = document.querySelector(".html5-video-player")
+            const video = document.querySelector(".video-stream.html5-main-video")
+            function CreateProgressEvent() {
+                if (!video || !player) {
+                    requestAnimationFrame(CreateProgressEvent)
+                }
+                else if (!player.classList.contains("progress-event")) {
+                    video.addEventListener("timeupdate", timeup)
+                    video.addEventListener("progress", progup)
+                    player.classList.add("progress-event");
+                } else {
+                    $('.DummyPlayProgress').attr({style: "transform: scaleX(0);"});
+                    $('.DummyLoadProgress').attr({style: "transform: scaleX(0);"});
+                }
+            }
+            requestAnimationFrame(CreateProgressEvent)
         }
-        requestAnimationFrame(findVideoInterval)
-    }
-    // Video Time Left Counter
-    // Created by Gentle
-    if(window.location.href.indexOf("watch") !==-1 ) {
+        requestAnimationFrame(UpdateMiniBar)
+
+        // Video Time Left Counter
         function createTime() {
             const cont = document.querySelector('.VTLC')
             if (cont) {
                 UpdateTime();
             }
             if (!cont) {
-                const container2 = $('<div>').addClass('VTLC').css({display: 'block','min-width': 'fit-content',overflow: 'hidden','font-weight': 500,color: '#f1f1f1','font-size': '14px','line-height': '36px','background-color': 'var(--yt-spec-badge-chip-background)',padding: '0px 10px','border-radius': '2px','margin-left': '8px'})
+                const container2 = $('<div>').addClass('VTLC')
                 const subbutton = document.querySelector('#owner > #subscribe-button')
                 $(container2).insertAfter(subbutton);
                 requestAnimationFrame(createTime)
@@ -83,16 +100,44 @@ window.addEventListener("yt-page-data-updated", function(e) {
             return `${hours}:${minutes}:${seconds}`
         }
         function UpdateTime() {
-            function vtlc() {$('.VTLC').text(format(video.duration - video.currentTime, video.playbackRate))}
-            document.querySelector(".video-stream").removeEventListener("timeupdate", vtlc, true);
-            const video = document.querySelector(".video-stream.html5-main-video");
+            function vtlc() {$('.VTLC').text(format(document.querySelector(".video-stream.html5-main-video").duration - document.querySelector(".video-stream.html5-main-video").currentTime, document.querySelector(".video-stream.html5-main-video").playbackRate))}
+            const player = document.querySelector(".html5-video-player")
+            const video = document.querySelector(".video-stream.html5-main-video")
             const cont = document.querySelector('.VTLC')
             function VideoTime() {
-                if (!video || !cont) {requestAnimationFrame(VideoTime)}
-                video.addEventListener("timeupdate", vtlc, true);
+                if (!video || !cont) {
+                    requestAnimationFrame(VideoTime)
+                }
+                if (!player.classList.contains("time-event")) {
+                    video.addEventListener("timeupdate", vtlc)
+                    player.classList.add("time-event");
+                }
+                else {
+                    requestAnimationFrame(VideoTime)
+                }
             }
             requestAnimationFrame(VideoTime)
         }
         requestAnimationFrame(createTime)
     }
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
