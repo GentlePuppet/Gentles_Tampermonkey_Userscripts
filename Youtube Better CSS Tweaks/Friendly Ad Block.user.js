eb7b26c0c9150a00062efbbeb07c5f8e58ec5d46
Wruzz Jen-Iisk <43224790+GentlePuppet@users.noreply.github.com>
2023-11-22 00:37:09 -0500

    Update Friendly Ad Block.user.js

--- Youtube Better CSS Tweaks/Friendly Ad Block.user.js
+++ Youtube Better CSS Tweaks/Friendly Ad Block.user.js
@@ -1,18 +1,27 @@
 // ==UserScript==
-// @name         Gentle's Ad Block
-// @version      1.6.1
-// @author       Originally by 0x48piraj | Converted to a simpified userscript by Gentle
+// @name         Gentle's Ad Skipper
+// @version      1.7
+// @author       GentlePuppet
 // @match        https://www.youtube.com/*
 // @icon         https://www.youtube.com/s/desktop/1eca3218/img/favicon_144.png
-// @description  Friendly Ad Block is an extension created by 0x48piraj @https://github.com/0x48piraj/fadblock/tree/master. Converted into a simple userscript by Gentle.
+// @description  A simple script to instantly skip any ads that start playing. It also hides various static ads.
+// @grant        GM_addStyle
 // @require      https://code.jquery.com/jquery-3.7.1.min.js
 // @run-at       document-start
 // @updateURL    https://github.com/GentlePuppet/Gentles_Tampermonkey_Userscripts/raw/main/Youtube%20Better%20CSS%20Tweaks/Friendly%20Ad%20Block.user.js
 // @downloadURL  https://github.com/GentlePuppet/Gentles_Tampermonkey_Userscripts/raw/main/Youtube%20Better%20CSS%20Tweaks/Friendly%20Ad%20Block.user.js
 // ==/UserScript==
-
+GM_addStyle(`
+    .ytd-companion-slot-renderer, .ytd-action-companion-ad-renderer,
+    .ytd-watch-next-secondary-results-renderer.sparkles-light-cta, .ytd-unlimited-offer-module-renderer,
+    .ytp-ad-overlay-image, .ytp-ad-text-overlay,
+    div#root.style-scope.ytd-display-ad-renderer.yt-simple-endpoint, div#sparkles-container.style-scope.ytd-promoted-sparkles-web-renderer,
+    .ytd-display-ad-renderer, .ytd-statement-banner-renderer, .ytd-in-feed-ad-layout-renderer,
+    div#player-ads.style-scope.ytd-watch-flexy, ytd-engagement-panel-section-list-renderer[target-id='engagement-panel-ads'],
+    .ytd-banner-promo-renderer, .ytd-video-masthead-ad-v3-renderer, .ytd-primetime-promo-renderer
+    {display: none !important;}
+`);
 let TimerActive = false
-
 function ClickAdButtons() {document.querySelectorAll(".ytp-ad-skip-button, .ytp-ad-skip-button-modern").forEach(b => {if (b !== null) {b.click()}})}
 function AdTimer() {
     if (!TimerActive) {
@@ -23,10 +32,9 @@ function AdTimer() {
                 if (document.querySelector('#movie_player > div.html5-video-container > video').currentTime > 0) {
                     document.querySelector('#movie_player > div.html5-video-container > video').currentTime = Math.max(0, document.querySelector('#movie_player > div.html5-video-container > video').duration - 0.1)
                     document.querySelector('#movie_player > div.html5-video-container > video').paused && document.querySelector('#movie_player > div.html5-video-container > video').play()
+                    document.querySelector('#movie_player > div.html5-video-container > video').muted = false
                 }
                 ClickAdButtons()
-            } else {
-                document.querySelector('#movie_player > div.html5-video-container > video').muted = false
             }
             requestAnimationFrame(update)
         }
@@ -34,18 +42,3 @@ function AdTimer() {
     }
 }
 window.addEventListener("yt-page-data-updated", function() {AdTimer()})
-
-window.addEventListener("yt-page-data-updated", function(e) {
-    function HideAdLoop() {
-        const staticAds = [".ytd-companion-slot-renderer", ".ytd-action-companion-ad-renderer", // in-feed video ads
-                           ".ytd-watch-next-secondary-results-renderer.sparkles-light-cta", ".ytd-unlimited-offer-module-renderer", // similar components
-                           ".ytp-ad-overlay-image", ".ytp-ad-text-overlay", // deprecated overlay ads (04-06-2023)
-                           "div#root.style-scope.ytd-display-ad-renderer.yt-simple-endpoint", "div#sparkles-container.style-scope.ytd-promoted-sparkles-web-renderer",
-                           ".ytd-display-ad-renderer", ".ytd-statement-banner-renderer", ".ytd-in-feed-ad-layout-renderer", // homepage ads
-                           "div#player-ads.style-scope.ytd-watch-flexy, div#panels.style-scope.ytd-watch-flexy", // sponsors
-                           ".ytd-banner-promo-renderer", ".ytd-video-masthead-ad-v3-renderer", ".ytd-primetime-promo-renderer" // subscribe for premium & youtube tv ads
-                          ]
-        staticAds.forEach((ad) => {document.querySelectorAll(ad).forEach((el) => (el.style.display = "none"))})
-    }
-    setTimeout(HideAdLoop, 1000)
-})
