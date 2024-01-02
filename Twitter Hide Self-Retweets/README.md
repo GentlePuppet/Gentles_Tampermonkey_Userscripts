a39b471abb993cfcad0487b12447338bb9b88e72
Wruzz Jen-Iisk <43224790+GentlePuppet@users.noreply.github.com>
2024-01-02 03:01:14 -0500

    Delete Twitter Hide Self-Retweets directory

--- Twitter Hide Self-Retweets/README.md
+++
@@ -1,8 +0,0 @@
-# Gentle's Twitter Hide Self-Retweets
-
-## Description:
-This script as been merged with my Better Twitter Script.
-
-
-Check it out here:
-* [Better Twitter](https://github.com/GentlePuppet/Gentles_Tampermonkey_Userscripts/tree/main/Better%20Twitter)
--- Twitter Hide Self-Retweets/Twitter_Remove_Self-Retweets.user.js
+++
@@ -1,8 +0,0 @@
-// ==UserScript==
-// @name         Twitter Remove Self-Retweets
-// @author       GentlePuppet
-// @version      Removed
-// ==/UserScript==
-
-// Script merged with "https://github.com/GentlePuppet/Gentles_Tampermonkey_Userscripts/raw/main/Better%20Twitter/Better%20Twitter.user.js"
-// Please Delete this Script
