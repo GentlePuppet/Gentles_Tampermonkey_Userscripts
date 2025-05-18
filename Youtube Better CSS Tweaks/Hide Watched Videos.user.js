c59981af4da356219ee6c22dadd36001ea2c8e6f
Wruzz Jen-Iisk <43224790+GentlePuppet@users.noreply.github.com>
2025-05-17 20:41:10 -0400

    Update Hide Watched Videos.user.js
    Long past due update:
    - Removed the jquery cookies require
    - Changed jquery.cookie to document.cookie
    - Cleaned up the code

--- Youtube Better CSS Tweaks/Hide Watched Videos.user.js
+++ Youtube Better CSS Tweaks/Hide Watched Videos.user.js
@@ -1,98 +1,119 @@
 // ==UserScript==
 // @name         Youtube Hide Watched Videos
 // @author       GentlePuppet
-// @version      1.4.4
+// @version      1.5
 // @match        https://www.youtube.com/*
 // @icon         https://www.youtube.com/s/desktop/1eca3218/img/favicon_144.png
-// @require      https://code.jquery.com/jquery-3.5.1.min.js
+// @require      https://code.jquery.com/jquery-3.7.1.min.js
 // @require      https://gist.github.com/raw/2625891/waitForKeyElements.js
-// @require      https://raw.githubusercontent.com/carhartl/jquery-cookie/v1.4.1/jquery.cookie.js
 // @grant        GM_addStyle
-// @run-at       document-start
-// @updateURL    https://github.com/GentlePuppet/Gentles_Tampermonkey_Userscripts/raw/main/Youtube%20Better%20CSS%20Tweaks/Hide%20Watched%20Videos.user.js
-// @downloadURL  https://github.com/GentlePuppet/Gentles_Tampermonkey_Userscripts/raw/main/Youtube%20Better%20CSS%20Tweaks/Hide%20Watched%20Videos.user.js
+// @updateURL    https://github.com/GentlePuppet/Gentles_Tampermonkey_Userscripts/raw/main/Youtube%20Hide%20Watched%20Videos/Youtube%20Hide%20Watched%20Videos.user.js
+// @downloadURL  https://github.com/GentlePuppet/Gentles_Tampermonkey_Userscripts/raw/main/Youtube%20Hide%20Watched%20Videos/Youtube%20Hide%20Watched%20Videos.user.js
 // ==/UserScript==
 // Watched Video Show/Hide Button Stylings and Dim Watched Videos
 GM_addStyle(`
-    .WatchedVideoButton {align-self: normal !important;color: var(--yt-spec-text-primary) !important;overflow: hidden !important;font-size: var(--ytd-subheadline-link_-_font-size) !important;font-weight: var(--ytd-subheadline-link_-_font-weight) !important;line-height: var(--ytd-subheadline-link_-_line-height) !important;letter-spacing: var(--ytd-subheadline-link_-_letter-spacing) !important;background: #383838 !important;border: black 1px solid;cursor: pointer;text-shadow: 1px 1px 3px black;}
+    .WatchedVideoButton {height: 30px;margin: auto;align-self: normal !important;color: var(--yt-spec-text-primary) !important;overflow: hidden !important;font-family: "Roboto","Arial",sans-serif !important;font-size: 1.4rem !important;line-height: 2rem !important;font-weight: 400 !important;background: #383838 !important;border: black 1px solid;cursor: pointer;text-shadow: 1px 1px 3px black;}
     .WatchedVideoButton:hover {background: #595959 !important;}
-    .Watched_Video_Shown {Opacity: 80%; background: #271b38; border: 2px #004eff solid; padding: 5px; }
+    .WatchedVideosNumberlabel {padding: 0px 5px 0px 5px;color: var(--yt-spec-text-primary) !important; background: #720586 !important; border: black 1px solid; height: 28px;margin: auto;align-self: normal; text-shadow: 1px 1px 3px black;font-family: "Roboto","Arial",sans-serif !important;font-size: 1.4rem !important; line-height: 28px !important;letter-spacing: var(--ytd-subheadline-link_-_letter-spacing) !important;}.Watched_Video_Shown {Opacity: 80%; background: #271b38; border: 2px #004eff solid; padding: 5px; }
     .Watched_Video_Shown > div > ytd-thumbnail {Opacity: 40%; }
     .Watched_Video_Hidden {Opacity: 80%; background: #271b38; border: 2px #004eff solid; padding: 5px; }
     .Watched_Video_Hidden > div > ytd-thumbnail {Opacity: 40%; }
     ytd-continuation-item-renderer {height: 0px !important;}
     paper-spinner.ytd-continuation-item-renderer {display: none !important; margin: 0px !important;}
     .ytp-spinner {display: none !important;}
+    #columns.ytd-watch-flexy {overflow: hidden !important;}
 `);
+
 window.addEventListener("yt-page-data-updated", function(e) {
-    var wvh = null;
-    var wvs = null;
-    delete wvh
-    delete wvs
-    $("#WatchedVideosNumber").text("0");
+    // $("#WatchedVideosNumber").text("0");
     if(window.location.href.indexOf("playlist") == -1 || window.location.href.indexOf("history") == -1) {
-        // Variables
-        var HideWatchedVideosButton = document.querySelector("#HideWatchedVideosButton");
-        var ShowWatchedVideosButton = document.querySelector("#ShowWatchedVideosButton");
-        var HiddenVideosCounterCheck = document.querySelector("#WatchedVideosNumber");
-        // Hide Watched Videos every 2.5 Seconds
+        // Check Cookie
+        function getHideWatchedPref() {
+            const cookies = document.cookie.split('; ');
+            const cookie = cookies.find(row => row.startsWith('hidewatchedvideos='));
+
+            if (!cookie) {
+                setHideWatchedPref('1'); // Default to hiding watched videos
+                return '1';
+            }
+
+            return cookie.split('=')[1];
+        }
+
+        // Set Cookie
+        function setHideWatchedPref(value) {
+            document.cookie = `hidewatchedvideos=${value}; path=/; domain=.youtube.com; max-age=604800`;
+        }
+
+        // Hide Watched Videos
+        function handleNewVideos() {
+            const selectorMap = [
+                "ytd-grid-video-renderer",
+                "ytd-compact-video-renderer",
+                "ytd-rich-item-renderer"
+            ];
+            const shouldHide = getHideWatchedPref() === '1';
+
+            let hiddenCount = 0;
+            let shownCount = 0;
+
+            // Mark videos as watched (adds class only once)
+            $('div[id="progress"]').each(function () {
+                const video = $(this).parents(selectorMap.join(',')).first();
+                if (video.length > 0) {
+                    video.addClass('Watched_Video');
+                }
+            });
+
+            // Hide or show based on current toggle state
+            $('.Watched_Video').each(function () {
+                if (shouldHide) {
+                    $(this).hide().attr("class", "Watched_Video Watched_Video_Hidden");
+                    hiddenCount++;
+                } else {
+                    $(this).show().attr("class", "Watched_Video Watched_Video_Shown");
+                    shownCount++;
+                }
+            });
+
+            $("#WatchedVideosNumber").text(hiddenCount + shownCount);
+        }
         setInterval(function() {
-            if($.cookie('hidewatchedvideos', Number) == 1) {
-                $.cookie('hidewatchedvideos', '1', { domain: '.youtube.com', expires: 7, path: '/' });
-                $('div[id="progress"]').parents("ytd-grid-video-renderer").attr("class", "Watched_Video_Hidden").hide();
-                $('div[id="progress"]').parents("ytd-compact-video-renderer").attr("class", "Watched_Video_Hidden").hide();
-                $('div[id="progress"]').parents("ytd-rich-item-renderer").attr("class", "Watched_Video_Hidden").hide();
-                $('ytd-grid-video-renderer').find('ytd-thumbnail-overlay-resume-playback-renderer').remove();
-                $('ytd-compact-video-renderer').find('ytd-thumbnail-overlay-resume-playback-renderer').remove();
-                $('ytd-rich-item-renderer').find('ytd-thumbnail-overlay-resume-playback-renderer').remove();
-            }
-            if($.cookie('hidewatchedvideos', Number) == 0) {
-                $.cookie('hidewatchedvideos', '0', { domain: '.youtube.com', expires: 7, path: '/' });
-                $('div[id="progress"]').parents("ytd-grid-video-renderer").attr("class", "Watched_Video_Shown");
-                $('div[id="progress"]').parents("ytd-compact-video-renderer").attr("class", "Watched_Video_Shown");
-                $('div[id="progress"]').parents("ytd-rich-item-renderer").attr("class", "Watched_Video_Shown");
-                $('ytd-grid-video-renderer').find('ytd-thumbnail-overlay-resume-playback-renderer').remove();
-                $('ytd-compact-video-renderer').find('ytd-thumbnail-overlay-resume-playback-renderer').remove();
-                $('ytd-rich-item-renderer').find('ytd-thumbnail-overlay-resume-playback-renderer').remove();
-            }
-            var wvh = $(".Watched_Video_Hidden").length;
-            var wvs = $(".Watched_Video_Shown").length;
-            $("#WatchedVideosNumber").text(wvh + wvs);
+            handleNewVideos();
         },2500);
+
         // Create Watched Videos Number Counter
         waitForKeyElements ('#start.ytd-masthead', CreateHiddenVideosCounter, 0);
         function CreateHiddenVideosCounter () {
-            if($.cookie('hidewatchedvideos') == undefined) {$.cookie('hidewatchedvideos', '1', { domain: '.youtube.com', expires: 7, path: '/' });}
-            if (!HiddenVideosCounterCheck) {
-                var ra123 = $('<div/>').attr({type: "div",id: "WatchedVideosNumber",class: "WatchedVideosNumberlabel",style: "color: var(--yt-spec-text-primary) !important; font-size: var(--ytd-subheadline-link_-_font-size) !important; background: #720586 !important; padding: 9px; border: black 1px solid; height: 19px; text-shadow: 1px black; text-shadow: 1px 1px 3px black;"});
+            getHideWatchedPref()
+            if (!document.querySelector("#WatchedVideosNumber")) {
+                var ra123 = $('<div/>').attr({type: "div",id: "WatchedVideosNumber",class: "WatchedVideosNumberlabel"});
                 $('#start.ytd-masthead').append(ra123);
                 $("#WatchedVideosNumber").text("0");
             }
         }
+
 		// Create Toggle Button
         waitForKeyElements ('#WatchedVideosNumber', CreateToggleButton, 0);
         function CreateToggleButton () {
             var r2 = $('<input/>').attr({type: "button",id: "HideWatchedVideosButton",value: "Toggle Watched Videos",class: "WatchedVideoButton"});
             $(r2).insertBefore('#WatchedVideosNumber');
             document.getElementById("HideWatchedVideosButton").addEventListener("click", ToggleClickAction, false);
-            if($.cookie('hidewatchedvideos', Number) == 1) {$('.WatchedVideoButton').attr('value', 'Show Watched Videos').attr('style', 'width: 175px;');}
-            if($.cookie('hidewatchedvideos', Number) == 0) {$('.WatchedVideoButton').attr('value', 'Hide Watched Videos').attr('style', 'width: 168px;');}
+            if(getHideWatchedPref() === '1') {$('.WatchedVideoButton').attr('value', 'Show Watched Videos').attr('style', 'width: 175px;');}
+            if(getHideWatchedPref() === '0') {$('.WatchedVideoButton').attr('value', 'Hide Watched Videos').attr('style', 'width: 168px;');}
         }
 		// Toggle Action
 		function ToggleClickAction() {
-            if($.cookie('hidewatchedvideos', Number) == 0) {
-				$.cookie('hidewatchedvideos', '1', { domain: '.youtube.com', expires: 7, path: '/' });
-				$('.Watched_Video_Shown').removeAttr('style');
-				$(".Watched_Video_Shown").each(function() {$(this).hide()});
-				$(".Watched_Video_Shown").each(function() {$(this).attr("class", "Watched_Video_Hidden")});
+            if(getHideWatchedPref() === '0') {
+				setHideWatchedPref('1');
 				$('.WatchedVideoButton').attr('value', 'Show Watched Videos').attr('style', 'width: 175px;');
+                handleNewVideos();
                 return
             }
-            if($.cookie('hidewatchedvideos', Number) == 1) {
-				$.cookie('hidewatchedvideos', '0', { domain: '.youtube.com', expires: 7, path: '/' });
-				$(".Watched_Video_Hidden").each(function() {$(this).show()});
-				$(".Watched_Video_Hidden").each(function() {$(this).attr("class", "Watched_Video_Shown")});
+            if(getHideWatchedPref() === '1') {
+				setHideWatchedPref('0');
 				$('.WatchedVideoButton').attr('value', 'Hide Watched Videos').attr('style', 'width: 168px;');
+                handleNewVideos();
                 return
             }
         }
