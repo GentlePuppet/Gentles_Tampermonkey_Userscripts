56c8dec684f697e35478966ba5f10fc26d69d2e2
Wruzz Jen-Iisk <43224790+GentlePuppet@users.noreply.github.com>
2025-07-01 16:47:13 -0400

    Create Dynamic Gain.user.js
    Temporary file to update the script correctly.

---
+++ Youtube Hide Watched Videos/Dynamic Gain.user.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         Youtube Gentle's Auto Gain
+// @author       GentlePuppet
+// @version      2.5.5
+// @description  This script automatically boosts quiet YouTube videos or lowers loud videos by automatically adjusting audio gain with smoothing.
+// @author       Special Thanks to this old extension I found and adapted some of their javascript: https://github.com/Kelvin-Ng/youtube-volume-normalizer
+// @grant        GM_addStyle
+// @include      https://www.youtube.com/*
+// @icon         https://www.youtube.com/s/desktop/1eca3218/img/favicon_144.png
+// @updateURL    https://github.com/GentlePuppet/Gentles_Tampermonkey_Userscripts/raw/refs/heads/main/Youtube%20Better%20CSS%20Tweaks/Dynamic%20Gain.user.js
+// @downloadURL  https://github.com/GentlePuppet/Gentles_Tampermonkey_Userscripts/raw/refs/heads/main/Youtube%20Better%20CSS%20Tweaks/Dynamic%20Gain.user.js
+// ==/UserScript==
+
+// Please check for an update, I had the wrong update url.
+// This file is temporary and will be deleted in a few days.
